9ac909d63cfa07966daaa664cff566bf9102b6f5
vladimir <drozdov_vova2110@mail.ru>
2023-07-17 18:41:00 +0500

    refactor(sliders): animation logic function

--- src/js/aboutSlider.js
+++ src/js/aboutSlider.js
@@ -1,25 +1,12 @@
+import { animateDown } from "./swiperDownAnimation";
+
 const aboutSlider = document.querySelector(".about__slider");
 if (aboutSlider) {
     const swiper = new Swiper(aboutSlider, {
       grabCursor: true,
       slidesPerView: 3,
       spaceBetween: 30,
-      // slidesWidth: 372,
-      // autoHeight: true,
-      // slidesPerGroup: 1,
     });
-    swiper.on("slideChangeTransitionStart", function (e) {
-      console.log(e, 'slide change begin');
-      const index = e.activeIndex - 1;
-      const slides = e.slides;
-      slides.forEach(elem => {
-        elem.style.transform = '';
-        elem.style.transition = '';
-      });
-      if (slides[index]) {
-        slides[index].style.transition = '200ms linear';
-        slides[index].style.transform = 'translateY(500px)';
-      }
-    })
+    animateDown(swiper);
 }
 
--- src/js/mainPageSlider.js
+++ src/js/mainPageSlider.js
@@ -1,16 +1,13 @@
+import { animateDown } from "./swiperDownAnimation";
+
 const mainSlider = document.querySelector(".gallery-projects__slider");
 if (mainSlider) {
     const swiper = new Swiper(mainSlider, {
       grabCursor: true,
       slidesPerView: 1,
-      // spaceBetween: 24,
       width: 404,
-      // slidesWidth: 372,
-      // autoHeight: true,
-      // slidesPerGroup: 1,
       navigation: {
         nextEl: ".gallery-projects__next",
-        // prevEl: ".swiper-button-prev",
       },
       pagination: {
         el: '.gallery-projects__pagination',
@@ -18,21 +15,8 @@ if (mainSlider) {
         bulletActiveClass: 'active',
         bulletClass: 'gallery-projects__bullet',
       }
-      // allowTouchMove: false,
     });
-    swiper.on("slideChangeTransitionStart", function (e) {
-      console.log(e, 'slide change begin');
-      const index = e.activeIndex - 1;
-      const slides = e.slides;
-      slides.forEach(elem => {
-        elem.style.transform = '';
-        elem.style.transition = '';
-      });
-      if (slides[index]) {
-        slides[index].style.transition = '200ms linear';
-        slides[index].style.transform = 'translateY(500px)';
-      }
-    })
+    animateDown(swiper);
 
     const btnsAlternate = mainSlider.querySelectorAll('.gallery-projects__alternate');
     btnsAlternate.forEach((btn) => {
