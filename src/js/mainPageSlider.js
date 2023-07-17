2c463ba5ed2d9c72147b197df982c657e5e571a4
vladimir <drozdov_vova2110@mail.ru>
2023-07-17 13:29:19 +0500

    feat(gallery-projects): add ui, styles, and slider logic

--- src/js/mainPageSlider.js
+++ src/js/mainPageSlider.js
@@ -3,17 +3,43 @@ console.log(mainSlider);
 if (mainSlider) {
     const swiper = new Swiper(mainSlider, {
       grabCursor: true,
-      //   direction: "vertical",
-      effect: "creative",
-      creativeEffect: {
-        prev: {
-          shadow: true,
-          translate: [0, 400, 0],
-        },
-        next: {
-          translate: ["100%", 0, 0],
-        },
+      slidesPerView: 1,
+      // spaceBetween: 24,
+      width: 404,
+      // slidesWidth: 372,
+      // autoHeight: true,
+      // slidesPerGroup: 1,
+      navigation: {
+        nextEl: ".gallery-projects__next",
+        // prevEl: ".swiper-button-prev",
       },
+      pagination: {
+        el: '.gallery-projects__pagination',
+        clickable: true,
+        bulletActiveClass: 'active',
+        bulletClass: 'gallery-projects__bullet',
+      }
+      // allowTouchMove: false,
     });
+    swiper.on("slideChangeTransitionStart", function (e) {
+      console.log(e, 'slide change begin');
+      const index = e.activeIndex - 1;
+      const slides = e.slides;
+      slides.forEach(elem => {
+        elem.style.transform = '';
+        elem.style.transition = '';
+      });
+      if (slides[index]) {
+        slides[index].style.transition = '200ms linear';
+        slides[index].style.transform = 'translateY(500px)';
+      }
+    })
+
+    const btnsAlternate = mainSlider.querySelectorAll('.gallery-projects__alternate');
+    btnsAlternate.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        swiper.slideNext();
+      })
+    })
 }
 
