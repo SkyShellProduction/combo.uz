daaae9091847485307a9feed00b03f5e803c7698
vladimir <drozdov_vova2110@mail.ru>
2023-07-18 11:52:18 +0500

    refactor(tabs): remove commented code

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -3,7 +3,6 @@ import { headerLinkOpen, modal } from "./modal";
 if (headerLinkOpen && modal) {
   const tabLinks = [...modal.querySelectorAll(".modal__tabs-link")];
   const tabItems = [...modal.querySelectorAll(".modal__items-elem")];
-  //   console.log(tabLinks, tabItems);
   tabLinks.forEach((link, i) => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
