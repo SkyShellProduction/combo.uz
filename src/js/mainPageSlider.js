8c7e7b7623d8567fb59cac788c278e994f7c2258
vova <drozdov_vova2110@mail.ru>
2023-08-04 21:27:31 +0500

    fix(main-page): add slider in the begining of page

--- src/js/mainPageSlider.js
+++ src/js/mainPageSlider.js
@@ -2,37 +2,48 @@ import { animateDown } from "./swiperDownAnimation";
 
 const mainSlider = document.querySelector(".gallery-projects__slider");
 if (mainSlider) {
-    const swiper = new Swiper(mainSlider, {
-      grabCursor: true,
-      slidesPerView: 1.2,
-      // spaceBetween: 30,
-      width: 230,
-      loop: true,
-      navigation: {
-        nextEl: ".gallery-projects__next",
+  const swiper = new Swiper(mainSlider, {
+    grabCursor: true,
+    slidesPerView: 1.2,
+    // spaceBetween: 30,
+    width: 230,
+    loop: true,
+    navigation: {
+      nextEl: ".gallery-projects__next",
+    },
+    pagination: {
+      el: ".gallery-projects__pagination",
+      clickable: true,
+      bulletActiveClass: "active",
+      bulletClass: "gallery-projects__bullet",
+    },
+    breakpoints: {
+      1016: {
+        width: 404,
       },
-      pagination: {
-        el: '.gallery-projects__pagination',
-        clickable: true,
-        bulletActiveClass: 'active',
-        bulletClass: 'gallery-projects__bullet',
+      680: {
+        width: 300,
       },
-      breakpoints: {
-        1016: {
-          width: 404,
-        },
-        680: {
-          width: 300
-        }
-      }
-    });
-    animateDown(swiper);
+    },
+  });
+  animateDown(swiper);
 
-    const btnsAlternate = mainSlider.querySelectorAll('.gallery-projects__alternate');
-    btnsAlternate.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        swiper.slideNext();
-      })
-    })
+  const btnsAlternate = mainSlider.querySelectorAll(
+    ".gallery-projects__alternate"
+  );
+  btnsAlternate.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      swiper.slideNext();
+    });
+  });
 }
 
+const beginSlider = document.querySelector(".best-curtains__slider");
+if (beginSlider) {
+  new Swiper(beginSlider, {
+    grabCursor: true,
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    loop: true,
+  });
+}
