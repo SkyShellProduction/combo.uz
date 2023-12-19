7621e0f3e7f73a831f1a6e05dfb2b27a17576827
vladimir <drozdov_vova2110@mail.ru>
2023-12-19 16:34:25 +0500

    fix: add csrf token

--- src/js/api-client.js
+++ src/js/api-client.js
@@ -1,8 +1,12 @@
 export async function httpClient(url, method, params) {
+    const csrf = document.querySelector("input[name=csrfmiddlewaretoken]")?.getAttribute("name") || "";
     const baseUrl = "https://combouz.pythonanywhere.com";
     const f = await fetch(`${baseUrl}${url}`, {
         method,
         ...params,
+        headers: {
+            "X-CSRFToken": csrf,
+        }
     });
     if(f.ok) return await f.json();
     else throw new Error(`Cannot ${method} data from ${url}`);
